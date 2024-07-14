ad70ce5ed8e9b021143c32915b43878c4bef9256
florent marmol <florent.marmol@gmail.com>
2024-07-14 11:31:39 +0200

    initial commit

---
+++ table.mjs
@@ -0,0 +1,34 @@
+function setTableWidth(table) {
+    const columns = table.rows[0].cells.length;
+    const maxWidths = Array(columns).fill(0);
+
+    // Calculate the maximum width of each column
+    for (let row of table.rows) {
+        for (let i = 0; i < columns; i++) {
+            const cell = row.cells[i];
+            const cellWidth = cell.offsetWidth;
+            if (cellWidth > maxWidths[i]) {
+                maxWidths[i] = cellWidth;
+            }
+        }
+    }
+
+    // Set each column to the maximum width
+    for (let row of table.rows) {
+        for (let i = 0; i < columns; i++) {
+            row.cells[i].style.width = `${maxWidths[i]}px`;
+        }
+    }
+};
+
+
+function eventListener() {
+    document.addEventListener('DOMContentLoaded', function() {
+    let tables =  document.getElementsByClassName("table")
+    for (let table of tables) {
+      setTableWidth(table);
+    }
+  });
+};
+
+export { eventListener } 
